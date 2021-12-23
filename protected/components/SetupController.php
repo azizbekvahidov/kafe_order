0743b9b09295607c82f0abebedfb748f52daf91f
Азизбек Вахидов <vaxidov.aziz@loc.tashkent.uz>
2021-12-23 15:08:08 +0500

    add gitignore and remove license

--- protected/components/SetupController.php
+++ protected/components/SetupController.php
@@ -6,26 +6,26 @@ class SetupController extends Controller{
     
     public function __construct($id, $module = null)
     {   
-        $res = Yii::app()->db->CreateCommand()
-            ->select()
-            ->from("license")
-            ->where("progType = 'waiter' and active = 1")
-            ->queryRow();
-        if(!empty($res)){
-//            echo "<pre>";
-//            print_r("some");
-//            echo "</pre>";
-            $name = Yii::app()->config->get("name");
-            $func = new Functions();
-            $SN = $func->UniqueMachineID("C");
-            $hash = $func->GetHash($name,$res["secretKey"],$SN);
-            if($res["hash"] != $hash){
-                $this->redirect("/configure/index");
-            }
-        }
-        else{
-            $this->redirect("/configure/index");
-        }
+//         $res = Yii::app()->db->CreateCommand()
+//             ->select()
+//             ->from("license")
+//             ->where("progType = 'waiter' and active = 1")
+//             ->queryRow();
+//         if(!empty($res)){
+// //            echo "<pre>";
+// //            print_r("some");
+// //            echo "</pre>";
+//             $name = Yii::app()->config->get("name");
+//             $func = new Functions();
+//             $SN = $func->UniqueMachineID("C");
+//             $hash = $func->GetHash($name,$res["secretKey"],$SN);
+//             if($res["hash"] != $hash){
+//                 $this->redirect("/configure/index");
+//             }
+//         }
+//         else{
+//             $this->redirect("/configure/index");
+//         }
         parent::__construct($id, $module);
     }
 
